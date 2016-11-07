6cd24321094f1db1c4e19e1fd5caad8638d7d696
Manuel Ortiz <manuel.ortiz@fluxit.com.ar>
2016-11-07 13:31:20 -0300

    cambio el servidor de mock porque no soportaba https

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 Proyecto ejemplo hecho en Angular para el trabajo final de ttps 2016
 
+## Requerido
+Instalar el plugin que deshabilita cors
+https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi
+
+**NOTA: esto es porque consumo servicios desde el front que no son del mismo origen (servicios mocks)**
+
 ## Preview
+
+### Requerido
+Instalar el plugin que deshabilita cors
+https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi
+
+**NOTA: esto es porque consumo servicios desde el front que no son del mismo origen (servicios mocks)**
 
 <https://ortizman.github.io/cartelera-info-site/#/cartelera>
--- js/cartelera/services/cartelera.service.js
+++ js/cartelera/services/cartelera.service.js
@@ -5,16 +5,16 @@ angular.module('myapp.cartelera')
 
   var getCartelera = function(anio){
     if (anio == 'primero') {
-      return $http.get('https://www.mocky.io/v2/581f9f633e00002825c08e9a');
+      return $http.get('https://mockbin.org/bin/fbf0fd99-1eee-4a07-8644-21b6b3ef0052');
     }
     else if (anio == 'segundo') {
-      return $http.get('https://www.mocky.io/v2/581f9f633e00002825c08e9a');
+      return $http.get('http://mockbin.org/bin/d4b2771d-347a-4f7e-aa74-0566603a4fd3');
     } else if (anio == 'tercero') {
-
+      return $http.get('http://mockbin.org/bin/daf0cf4b-3341-4e50-b78b-aecbc89640de');
     } else if (anio == 'cuarto') {
-
+      return $http.get('http://mockbin.org/bin/37543bc3-fa45-47d5-87e5-d37cd4de7f16');
     } else {
-
+      return $http.get('http://mockbin.org/bin/000b8b36-d20c-43eb-8103-fcad0c6c61de');
     }
   };
 
