41c7c038971abefe6dba34d8ed9cf810f46038ca
Manuel Ortiz <manuel.ortiz@fluxit.com.ar>
2016-11-07 13:33:15 -0300

    cambio http a https

--- js/cartelera/services/cartelera.service.js
+++ js/cartelera/services/cartelera.service.js
@@ -8,13 +8,13 @@ angular.module('myapp.cartelera')
       return $http.get('https://mockbin.org/bin/fbf0fd99-1eee-4a07-8644-21b6b3ef0052');
     }
     else if (anio == 'segundo') {
-      return $http.get('http://mockbin.org/bin/d4b2771d-347a-4f7e-aa74-0566603a4fd3');
+      return $http.get('https://mockbin.org/bin/d4b2771d-347a-4f7e-aa74-0566603a4fd3');
     } else if (anio == 'tercero') {
-      return $http.get('http://mockbin.org/bin/daf0cf4b-3341-4e50-b78b-aecbc89640de');
+      return $http.get('https://mockbin.org/bin/daf0cf4b-3341-4e50-b78b-aecbc89640de');
     } else if (anio == 'cuarto') {
-      return $http.get('http://mockbin.org/bin/37543bc3-fa45-47d5-87e5-d37cd4de7f16');
+      return $http.get('https://mockbin.org/bin/37543bc3-fa45-47d5-87e5-d37cd4de7f16');
     } else {
-      return $http.get('http://mockbin.org/bin/000b8b36-d20c-43eb-8103-fcad0c6c61de');
+      return $http.get('https://mockbin.org/bin/000b8b36-d20c-43eb-8103-fcad0c6c61de');
     }
   };
 
